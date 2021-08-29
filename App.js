b655588858a9858d2420a14b2621f1edd7f7ec14
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-08-29 19:47:02 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,70 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  Button,
+} from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Reps & Weight</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="email" />
+        <TextInput style={styles.input} placeholder="password" />
+      </View>
+      <View style={styles.footer}>
+        <Button
+          title="Create Account"
+          accessibilityLabel="Create account button"
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#232323",
+    alignItems: "center",
+  },
+  titleContainer: {
+    flex: 0.1,
+    alignItems: "center",
+    borderWidth: 5,
+    width: "100%",
+  },
+  title: {
+    color: "#f0f0f0",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    flex: 0.75,
+    width: "100%",
+    paddingLeft: 32,
+    paddingRight: 32,
+  },
+  footer: {
+    flex: 0.15,
+    backgroundColor: "#b43919",
+    width: "100%",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  input: {
+    backgroundColor: "white",
+    width: "100%",
+    height: 60,
+    marginBottom: 16,
+    paddingLeft: 16,
+    borderRadius: 16,
+  },
+});
